25a11574d9628f85508bba509cf5a86148960da9
Vicky8180 <vyadav99x1@gmail.com>
2025-02-02 22:05:24 +0530

    seond

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,31 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const allowedOrigins = [
+    "http://localhost:3000",
+  "http://skc-anoopyadav-assignment.vercel.app/"
+  
+  ];
+  
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,DELETE,OPTIONS,PATCH"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+    if (req.method === "OPTIONS") {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+
 app.use("/api/ai", aiRoutes);
 
 const PORT = process.env.PORT || 5000;
